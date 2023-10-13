0e1cf7fed8d644554e89403830319aef5bdd8bf9
ivan <ivan@DESKTOP-NELLSAH>
2023-10-13 21:22:45 +0300

    done task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n > 0) { }
+    else
+        {n=-n}
+
+
+
+
+    let array = ("" + n).split("").map(Number);
+    console.log(array);
+
+
+
+    const reversed = array.reverse();
+    return +reversed.join("");
+};
